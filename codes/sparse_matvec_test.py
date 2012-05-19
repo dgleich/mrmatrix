32e569c6409cb2628d3aa27c5068e40bbc929197
David Gleich <dgleich@purdue.edu>
2012-05-19 15:18:50 -0700

    Sparse test data

---
+++ codes/sparse_matvec_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+def main():
+  vec = {}
+  mat = {}
+  for line in sys.stdin:
+    valarray = [float(v) for v in line.split()]
+    if len(colsums) < len(valarray):
+      colsums.extend([0. for _ in xrange(len(valarray) - len(colsums))])
+    for col,val in enumerate(valarray):
+      colsums[col] += val
+  for col,sum in enumerate(colsums):
+    print col, sum
+
+if __name__=='__main__':
+  main()
